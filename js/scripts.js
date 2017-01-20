416c1db08c66eafe37732b12ef8a0f8a9961bfde
keely glenn <glenj392@newschool.edu>
2017-01-20 09:29:36 -0800

    make the user input return a full number not individual numbers seperated by commons. YAY

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,31 @@
 // backend
 var returnNumber = function(userInput) {
-  var numbersArray = ["0", "1" ,"2", "3", "4", "5", "6", "7", "8", "9"];
+  var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
   var isNumber = true;
-  var userInputSplit = ($("#input").val()).split("");
+  var userInputSplit = userInput.split("");
 
 // is a number?
   userInputSplit.forEach(function(number){
     var numQuery = numbersArray.indexOf(number);
     if (numQuery === -1) {
-      isNumber= false;
+      isNumber = false;
     }
   });
 
   if (!isNumber) {
     return "Your input is invalid. Please only enter numbers";
   } else {
-    return "Your number is " + userInputSplit;
+    return "Your number is " + userInput;
   }
-
-}
+};
 
 
 // frontend
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
     var userInput = $("#input").val();
-    var answer = returnNumber(userInput);
-
-    $("#answer").text(returnNumber);
-  })
+    var result = returnNumber(userInput);
+    $("#answer").text(result);
+  });
 });
