ee0afa7136a62755df311f4b321aa011fcd9907f
keely glenn <glenj392@newschool.edu>
2017-01-20 10:36:29 -0800

    add if statement to change single digit numbers that divide by 3,5,15, to return with appropriate word. DOES NOT LIST NUMBER OUT INDIVIDUALLY. DOES NOT USE LOOP. Working on those next

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,26 @@
 var returnNumber = function(userInput) {
   var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
   var isNumber = true;
-  var userInputSplit = userInput.split("");
+  var userInput = parseInt($("#input").val());
+  var userInputSplit = userInput.toString().split("");
 
-// is a number?
+
+
+// number input change
+  var singleConvert = function(userInput) {
+    var singleDigit = parseInt(userInputSplit[userInputSplit.length -1]);
+    if (singleDigit % 3 === 0) {
+      return "Ping";
+    } else if (singleDigit % 5 === 0) {
+      return "Pong";
+    } else if (singleDigit % 5 === 0) {
+       return "Ping-Pong";
+    } else {
+      return userInput;
+    }
+  }
+
+  // is a number?
   userInputSplit.forEach(function(number){
     var numQuery = numbersArray.indexOf(number);
     if (numQuery === -1) {
@@ -15,16 +32,15 @@ var returnNumber = function(userInput) {
   if (!isNumber) {
     return "Your input is invalid. Please only enter numbers";
   } else {
-    return "Your number is " + userInput;
+    return "Your number is " + singleConvert(userInput);
   }
 };
-
 
 // frontend
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#input").val();
+    var userInput = parseInt($("#input").val());
     var result = returnNumber(userInput);
     $("#answer").text(result);
   });
