c444bab2b5bd752b3d3fe036d71f23e6fbfa3686
keely glenn <glenj392@newschool.edu>
2017-01-20 10:52:16 -0800

    use a loop to return single digit 3,5,15 to strings

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,34 @@ var returnNumber = function(userInput) {
 
 
 // number input change
-  var singleConvert = function(userInput) {
-    var singleDigit = parseInt(userInputSplit[userInputSplit.length -1]);
-    if (singleDigit % 3 === 0) {
-      return "Ping";
-    } else if (singleDigit % 5 === 0) {
-      return "Pong";
-    } else if (singleDigit % 5 === 0) {
-       return "Ping-Pong";
-    } else {
-      return userInput;
+  var multiConvert = function(userInput) {
+    var answer = [];
+    var anyDigit = userInputSplit;
+    for (i = 0; i < anyDigit.length; i++) {
+      if (anyDigit[i] % "3" === 0) {
+        return anyDigit[i] = "Ping";
+      }  else if (anyDigit[i] % "5" === 0) {
+        return anyDigit[i] = "Pong";
+      } else if (anyDigit[i] % "15" === 0) {
+        return anyDigit[i] = "Ping-Pong";
+      } else {
+        return userInput;
+      }
     }
   }
+
+  // var singleConvert = function(userInput) {
+  //   var singleDigit = parseInt(userInputSplit[userInputSplit.length -1]);
+  //   if (singleDigit % 3 === 0) {
+  //     return "Ping";
+  //   } else if (singleDigit % 5 === 0) {
+  //     return "Pong";
+  //   } else if (singleDigit % 5 === 0) {
+  //      return "Ping-Pong";
+  //   } else {
+  //     return userInput;
+  //   }
+  // }
 
   // is a number?
   userInputSplit.forEach(function(number){
@@ -32,7 +48,7 @@ var returnNumber = function(userInput) {
   if (!isNumber) {
     return "Your input is invalid. Please only enter numbers";
   } else {
-    return "Your number is " + singleConvert(userInput);
+    return "Your number is " + multiConvert(userInput);
   }
 };
 
