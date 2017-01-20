331fff1bd5fd84b4537adb397cc84cc57e7ab422
keely glenn <glenj392@newschool.edu>
2017-01-20 09:04:25 -0800

    fulfill the spec which determines if the input is a number or a word. returns invalid if a word and returns the numbers split if number

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
 // backend
-var returnNumber = function(number) {
+var returnNumber = function(userInput) {
+  var numbersArray = ["0", "1" ,"2", "3", "4", "5", "6", "7", "8", "9"];
+  var isNumber = true;
+  var userInputSplit = ($("#input").val()).split("");
+
+// is a number?
+  userInputSplit.forEach(function(number){
+    var numQuery = numbersArray.indexOf(number);
+    if (numQuery === -1) {
+      isNumber= false;
+    }
+  });
+
+  if (!isNumber) {
+    return "Your input is invalid. Please only enter numbers";
+  } else {
+    return "Your number is " + userInputSplit;
+  }
 
 }
-// frontend
 
+
+// frontend
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("#input").val());
-    var answer = returnNumber(number);
+    var userInput = $("#input").val();
+    var answer = returnNumber(userInput);
 
-    $("#answer").append(number);
+    $("#answer").text(returnNumber);
   })
 });
